4d6bfa32d44331ebf1cf15add0f2dca905c50988
propaganisa115 <iisanisajob@gmail.com>
2021-09-16 11:46:01 +0700

    update

--- scraping.py
+++ scraping.py
@@ -57,16 +57,16 @@ filterOnlydate = str(filterOnlydate).replace("'", '"')
 today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 for key in datas:
-    urlbnc = "https://" + str(key['sekolah_domain']) + "/api/main/backup"
+    urltopupstorage = "https://" + str(key['sekolah_domain']) + "/api/main/storage"
     if len(str(key['sekolah_domain'])) < 0:
-        log_scheduler_global = {"domain": "empty", "kode_scheduler": "scraping request of backup & cleansing data", "record_time": today,
+        log_scheduler_global = {"domain": "empty", "kode_scheduler": "scraping request of topup storage data", "record_time": today,
                                 "status": "failed", "error_message": "sekolah_domain is empty with id school " + str(
                 key['id']) + ", you can check in the table sekolah"}
         log_scheduler_global = str(log_scheduler_global).replace("'", '"')
         resp = requests.post(urlLogSchedulerGlobal, headers=headersPython2)
     else:
         try:
-            respClient = requests.get(urlbnc, headers=headersClient)
+            respClient = requests.get(urltopupstorage, headers=headersClient)
 
             if (respClient.status_code == 200):
 
@@ -79,54 +79,68 @@ for key in datas:
 
                 def ambilData(key):
                     data_topup_storage.append(
-                        {'id': str(key['id']), 'type': str(key['type']), 'proses_status': str(key['proses_status']),
-                         'title': str(key['title']), 'created_at': str(key['created_at']),
-                         'updated_at': str(key['updated_at']),
-                         'estimated_time': str(key['estimated_time']), 'link_backup': str(key['link_backup']),
+                        {'id': str(key['id']), 'idpaket': str(key['idpaket']), 'nama_paket': str(key['nama_paket']),
+                         'kapasitas': str(key['kapasitas']), 'bonus_kapasitas': str(key['bonus_kapasitas']),
+                         'harga': str(key['harga']),
+                         'harga_diskon': str(key['harga_diskon']), 'staff': str(key['staff'])
+                         ,'status': str(key['status']),'time_order': str(key['time_order'])
+                         ,'time_aktif': str(key['time_aktif']),'status_diskon': str(key['status_diskon'])
+                         , 'created_at': str(key['created_at']),
                          'sekolah_domain': str(domain)})
 
 
 
                 with ThreadPoolExecutor(max_workers=None) as exec:
-                    fut = [exec.submit(ambilData, key) for key in resp_json['data']]
+                    fut = [exec.submit(ambilData, key) for key in resp_json['list_storage']]
 
-                print(data_topup_storage)
+                #print(data_topup_storage)
 
-                urlbncserver = "https://api.seonindonesia.net/backup_cleansing/create"
+                urlTopup = "https://api.seonindonesia.net/tb_topup_storage/create"
                 headersPython = CaseInsensitiveDict()
                 headersPython["Accept"] = "application/json"
                 headersPython["Authorization"] = "token fc58379f2f373ea8a9a1535c642232ecdbf8a5ea"
                 headersPython["Content-Type"] = "application/json"
 
                 def tambahData(key):
-                    TambahData = {"domain_sekolah": key['sekolah_domain'], "id_backup": key['id'], "type": key['type'],
-                                  "proses_status": key['proses_status'], "title": key['title'], "created_at_client": key['created_at'],
-                                  "updated_at": key['updated_at'], "estimated_time": key['estimated_time'], "link_backup": key['link_backup']
-                                  ,"created_at_server": str(today)}
+                    TambahData = {"domain_sekolah": key['sekolah_domain'], "id_list_storage": key['id'], "idpaket": key['idpaket'],
+                                  "nama_paket": key['nama_paket'], "kapasitas": key['kapasitas'], "bonus_kapasitas": key['bonus_kapasitas'],
+                                  "harga": key['harga'], "harga_diskon": key['harga'], "staff": key['staff']
+                                  , "status": key['status'], "time_order": key['time_order']
+                                  , "time_aktif": key['time_aktif'], "status_diskon": key['status_diskon']
+                                  ,"created_at_client": key['created_at'],"created_at_server": str(today)}
                     tambahdatas = str(TambahData).replace("'", '"')
-                    print(tambahdatas)
-                    resp = requests.post(urlbncserver, headers=headersPython, data=tambahdatas)
-                    print(resp)
+                    #print(tambahdatas)
+                    resp = requests.post(urlTopup, headers=headersPython, data=tambahdatas)
+                    update = {"id":[key['id']]}
+                    update = str(update).replace("'", '"')
+                    print(update)
+                    try:
+                        respClient = requests.put(urltopupstorage, headers=headersClient,data=update)
+                        print(respClient)
+                    except Exception as e:
+                        error = str(e).replace("'", "")
+                        print(error)
+                    #print(resp)
 
 
                 with ThreadPoolExecutor(max_workers=None) as exec:
                     try:
                         futures = [exec.submit(tambahData, key) for key in data_topup_storage]
                         log_scheduler_global = {"domain": key['sekolah_domain'],
-                                                "kode_scheduler": "scraping request of backup & cleansing data", "record_time": today,
+                                                "kode_scheduler": "scraping request of topup storage data", "record_time": today,
                                                 "status": "success", "error_message": ""}
                         log_scheduler_global = str(log_scheduler_global).replace("'", '"')
                         resp = requests.post(urlLogSchedulerGlobal, headers=headersPython2, data=log_scheduler_global)
                     except Exception as e:
                         error = str(e).replace("'", "")
                         log_scheduler_global = {"domain": key['sekolah_domain'],
-                                                "kode_scheduler": "scraping request of backup & cleansing data", "record_time": today,
+                                                "kode_scheduler": "scraping request of topup storage data", "record_time": today,
                                                 "status": "failed", "error_message": error[:200]}
                         log_scheduler_global = str(log_scheduler_global).replace("'", '"')
                         resp = requests.post(urlLogSchedulerGlobal, headers=headersPython2, data=log_scheduler_global)
             else:
                 log_scheduler_global = {"domain": str(key['sekolah_domain']),
-                                        "kode_scheduler": "scraping request of backup & cleansing data", "record_time": today,
+                                        "kode_scheduler": "scraping request of topup storage data", "record_time": today,
                                         "status": "failed",
                                         "error_message": "response return http error with status code " + str(
                                             respClient.status_code)}
@@ -134,7 +148,7 @@ for key in datas:
                 resp = requests.post(urlLogSchedulerGlobal, headers=headersPython2, data=log_scheduler_global)
         except Exception as e:
             error = str(e).replace("'", "")
-            log_scheduler_global = {"domain": str(key['sekolah_domain']), "kode_scheduler": "scraping request of backup & cleansing data",
+            log_scheduler_global = {"domain": str(key['sekolah_domain']), "kode_scheduler": "scraping request of topup storage data",
                                     "record_time": today, "status": "failed", "error_message": error[:200]}
             log_scheduler_global = str(log_scheduler_global).replace("'", '"')
             resp = requests.post(urlLogSchedulerGlobal, headers=headersPython2, data=log_scheduler_global)
